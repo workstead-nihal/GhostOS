6877a917cdbdb4c8e11d4af80128c4eef3357190
Nihal Mollah <workstead.nihal@gmail.com>
2025-05-29 06:35:48 +0000

    Initial Setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__)
-
-from app import routes, models
+def create_app():
+    app = Flask(__name__)
+    from .routes import main
+    app.register_blueprint(main)
+    return app
--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,8 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('index.html')
 
 @main.route('/about')
 def about():
-    return render_template('base.html')  # You can create a separate template for the about page later.
+    return render_template('index.html')  # You can create a separate template for the about page later.
